5d72280433eaeb0d05f8d272977747512bdf83e7
gaurav-310 <gauravtripathi310@gmail.com>
2024-09-21 19:14:34 +0530

    Added solutions for 2024-09-21

---
+++ 2024-09-21/CF_problems/factorial.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#pragma GCC optimize("Ofast")
+
+#define mem(a, b)      memset(a, b, sizeof(a))
+#define nl             '\n'
+#define mod            1000000007
+#define MOD            998244353
+#define mod2           100000000
+#define pb             push_back
+#define mp             make_pair
+#define INF            4e18
+#define ff             first
+#define ss             second
+#define tt             third
+#define ppb            pop_back
+#define sz(x)          ((int)(x).size())
+#define pcnt(x)        __builtin_popcount(x)
+#define all(x)         (x).begin(), (x).end()
+#define sum(A)         accumulate(A.begin(), A.end(), 0)
+#define fr(i, s, e)    for(int i = s; i < e; i++)
+#define fore(e, v) for(auto &&e : v)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace std::chrono;
+
+typedef vector<int> vec;
+typedef vector<long long> vecl;
+typedef long long ll;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+const double pi = 3.1415926535;
+
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define dbg(x);
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*________________________________________________*/
+
+vecl sieve(ll n){vector<bool> prime(n+1, true); for (ll p = 2; p * p <= n; p++) if (prime[p] == true) for (ll i = p * p; i <= n; i += p) prime[i] = false; vecl v;
+for (ll p = 2; p <= n; p++){if (prime[p]) v.pb(p);} return v;}
+ll gcd(ll a, ll b){ if(b == 0){ return a;} return gcd(b, a%b);}
+ll nCr(int n, int r){ll p = 1, k = 1;if (n - r < r) {r = n - r;} if (r != 0) {while (r) {p *= n;k *= r; ll m = gcd(p, k);p /= m;k /= m;n--;r--;}}else {p = 1;} return p;}
+void google(int t){cout << "Case #" << t << ":" << " ";}
+void precision(int x){cout << fixed << setprecision(x);}
+ll fastexpo(ll x, ll y){ll res = 1; while (y) {if (y % 2 == 1)res = (res * x);y = y >> 1ll;x = (x * x);}return res;}
+ll fastexpomod(ll x, ll y, ll p) { ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0) { if (y & 1ll) res = (res*x) % p; y = y>>1ll;x = (x*x) % p; } return res; } 
+// ll nfactors(ll n){for(ll i = 2; i <= n; i++){for(ll j = i; j <= n; j += i)fact[j]++;}}
+ll modpar(ll a){return ((a%mod) + mod) % mod; }
+ll modadd(ll a,ll b){ return modpar(modpar(a)+modpar(b)); }
+ll modmul(ll a,ll b){ return modpar(modpar(a)*modpar(b)); }
+vecl fact;
+vecl pow_2;
+
+
+void fun(){
+    fact.resize(18);
+    pow_2.resize(35);
+
+    fact[0] = 1;
+    pow_2[0] = 1;
+
+    for (int i = 1; i < 18; ++i) {
+        fact[i] = fact[i-1] * i;
+    }
+
+    for (int i = 1; i < 35; ++i) {
+        pow_2[i] = pow_2[i-1] * 2;
+    }
+}
+ll one_bit(ll a){
+  ll count = 0;
+    while(a){
+        if(a&(1)) count++;
+        a = (a >> 1);
+    }
+    return count;
+}
+ll ans = INT_MAX;
+
+void fun1(ll val, ll ind = 15, ll c = 0){
+    if(ind <0) return;
+    if(val < 0) return;
+
+     ll one = one_bit(val);
+     ans = min(ans ,c + one);
+     fun1(val - fact[ind],ind-1,c+1);
+     fun1(val,ind-1,c);
+     
+
+}
+
+void solve(int test){
+    ll n;
+    cin>>n;
+    ans = INT_MAX;
+    fun1(n);
+    cout<<ans<<endl;
+
+ 
+}
+
+
+int main(){
+
+
+    precision(10); 
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.txt", "r", stdin);
+    freopen("outputf.txt", "w", stdout);
+    freopen("errorf.txt", "w", stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    auto start = high_resolution_clock::now();
+    int t = 1;
+
+    cin >> t;
+    fun();
+    int x = t;
+    while(t--){
+        solve(x - t);
+    }
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+      
+      #ifndef ONLINE_JUDGE
+        cout <<nl << "Time: "
+         << (float)duration.count()/1000000 << " s" << endl;
+        #endif
+}     
---
+++ 2024-09-21/CF_problems/range_query_point_update.cpp
@@ -0,0 +1,210 @@
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#pragma GCC optimize("Ofast")
+
+#define mem(a, b)      memset(a, b, sizeof(a))
+#define nl             '\n'
+#define mod            1000000007
+#define MOD            998244353
+#define mod2           100000000
+#define pb             push_back
+#define mp             make_pair
+#define INF            4e18
+#define ff             first
+#define ss             second
+#define tt             third
+#define ppb            pop_back
+#define sz(x)          ((int)(x).size())
+#define pcnt(x)        __builtin_popcount(x)
+#define all(x)         (x).begin(), (x).end()
+#define sum(A)         accumulate(A.begin(), A.end(), 0)
+#define fr(i, s, e)    for(int i = s; i < e; i++)
+#define fore(e, v) for(auto &&e : v)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace std::chrono;
+
+typedef vector<int> vec;
+typedef vector<long long> vecl;
+typedef long long ll;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+const double pi = 3.1415926535;
+
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define dbg(x);
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*________________________________________________*/
+
+vecl sieve(ll n){vector<bool> prime(n+1, true); for (ll p = 2; p * p <= n; p++) if (prime[p] == true) for (ll i = p * p; i <= n; i += p) prime[i] = false; vecl v;
+for (ll p = 2; p <= n; p++){if (prime[p]) v.pb(p);} return v;}
+ll gcd(ll a, ll b){ if(b == 0){ return a;} return gcd(b, a%b);}
+ll nCr(int n, int r){ll p = 1, k = 1;if (n - r < r) {r = n - r;} if (r != 0) {while (r) {p *= n;k *= r; ll m = gcd(p, k);p /= m;k /= m;n--;r--;}}else {p = 1;} return p;}
+void google(int t){cout << "Case #" << t << ":" << " ";}
+void precision(int x){cout << fixed << setprecision(x);}
+ll fastexpo(ll x, ll y){ll res = 1; while (y) {if (y % 2 == 1)res = (res * x);y = y >> 1ll;x = (x * x);}return res;}
+ll fastexpomod(ll x, ll y, ll p) { ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0) { if (y & 1ll) res = (res*x) % p; y = y>>1ll;x = (x*x) % p; } return res; } 
+// ll nfactors(ll n){for(ll i = 2; i <= n; i++){for(ll j = i; j <= n; j += i)fact[j]++;}}
+ll modpar(ll a){return ((a%mod) + mod) % mod; }
+ll modadd(ll a,ll b){ return modpar(modpar(a)+modpar(b)); }
+ll modmul(ll a,ll b){ return modpar(modpar(a)*modpar(b)); }
+vecl lazy;
+vecl tr;
+void propogate(ll l,ll r,ll ind){
+       if(lazy[ind] != 0){
+        dbg(ind);
+             tr[ind] = lazy[ind] + tr[ind];
+
+             if(l != r){
+                lazy[2*ind + 1] += 1;
+                lazy[2*ind + 2] += 1;
+             }
+            lazy[ind] = 0;
+       }
+}
+void modify(ll l,ll r,ll ind,ll i,ll j){
+     if(l == r){
+        tr[ind] = 1;
+        return;
+     }
+    if(tr[ind] >= 2) return;
+
+    if(r < i or l > j) return;
+
+    if(l >=i and r <= j){
+        lazy[ind] = tr[ind];
+        propogate(l,r,ind);
+        return;
+    }
+    ll mid = (l + r)/ 2;
+    modify(l , mid,2*ind + 1,i,j);
+    modify(mid + 1,r,2*ind + 2, i, j);
+    tr[ind] = tr[2*ind + 1] + tr[2*ind + 2];
+
+    return;
+    
+}
+ll value(ll l,ll r,ll ind, ll i){
+    if(l == r and r == i){
+        // tr[ind] = 1;
+        return tr[ind];
+    }
+    ll mid = (l + r) / 2;
+    ll a = value(l,mid,2*ind + 1,i);
+    ll b = value(mid+1,r,2*ind + 2,i);
+    
+    return  a + b;
+}
+ll modify(ll a){
+    ll ans = 0;
+    while(a){
+        ans += a%10;
+        a /=10;
+    }
+    return ans;
+}
+void solve(int test){
+    ll n,q;
+    cin>>n>>q;
+    vecl v(n);
+    set<ll> s;
+    fr(i,0,n) {
+ 
+        cin>>v[i];
+        if(v[i] > 9){
+            s.insert(i);
+        }
+        
+        }
+
+      
+    fr(i,0,q){
+        ll ty;
+        cin>>ty;
+        if(ty == 1){
+             ll l,r;
+             cin>>l>>r;
+             l--;
+             r--;
+             while(!s.empty()){
+                auto it = s.lower_bound(l);
+                if(it == s.end() or (*it) > r) break;
+                  ll value = modify(v[*it]);
+                  ll index = *it;
+                v[*it] = value;
+                if(value <= 9){
+                    s.erase(it);
+                }
+              
+                l = (index) + 1;
+              
+             }
+        }
+        else{
+            ll ind;
+            cin>>ind;
+            cout<<v[ind-1]<<nl;
+
+
+        }
+    }
+    
+
+ 
+}
+
+
+int main(){
+
+
+    precision(10); 
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.txt", "r", stdin);
+    freopen("outputf.txt", "w", stdout);
+    freopen("errorf.txt", "w", stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    auto start = high_resolution_clock::now();
+    int t = 1;
+
+    cin >> t;
+
+    int x = t;
+    while(t--){
+        solve(x - t);
+    }
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+      
+      #ifndef ONLINE_JUDGE
+        cout <<nl << "Time: "
+         << (float)duration.count()/1000000 << " s" << endl;
+        #endif
+}     
